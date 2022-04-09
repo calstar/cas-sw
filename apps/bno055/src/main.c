f4a72a6d1286eec9dac00c2c61b3bcc977a88f90
Cooper Collier <collier@berkeley.edu>
2022-04-08 19:33:20 -0700

    adding drivers for bno055

---
+++ apps/bno055/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/sensor.h>
+
+#define I2C_NODE DT_ALIAS(cas-onboard-i2c)
+
+void main(void) {
+	
+    const struct device *dev;
+
+}
