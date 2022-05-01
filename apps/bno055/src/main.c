c5c96c40dd4cacd6c516dccf7449f1d92a5e3cb4
Cedric Murphy <43464126+Andalite1999@users.noreply.github.com>
2022-05-01 16:36:50 -0700

    duped Adam's branch

--- apps/bno055/src/main.c
+++ apps/bno055/src/main.c
@@ -7,104 +7,45 @@
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
+#include <drivers/gpio.h>
+#include <sys/printk.h>
 #include <drivers/sensor.h>
 
-#define BNO055_I2C_BUS_WRITE_ARRAY_INDEX ((u8)1)
-#define I2C_BUFFER_LEN 8
-#define I2C0           5
-
-s8 BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt) {
-
-    s32 BNO055_iERROR = BNO055_INIT_VALUE;
-    u8 array[I2C_BUFFER_LEN] = { BNO055_INIT_VALUE };
-    u8 stringpos = BNO055_INIT_VALUE;
-
-    array[BNO055_INIT_VALUE] = reg_addr;
-
-    for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++) {
-        *(reg_data + stringpos) = array[stringpos];
-    }
-
-    return (s8)BNO055_iERROR;
-
-}
-
-s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt) {
-	
-	s32 BNO055_iERROR = BNO055_INIT_VALUE;
-    u8 array[I2C_BUFFER_LEN];
-    u8 stringpos = BNO055_INIT_VALUE;
-
-    array[BNO055_INIT_VALUE] = reg_addr;
-    for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++) {
-        array[stringpos + BNO055_I2C_BUS_WRITE_ARRAY_INDEX] = *(reg_data + stringpos);
-    }
-
-}
-
-s8 I2C_routine(void) {
-
-    bno055.bus_write = BNO055_I2C_bus_write;
-    bno055.bus_read = BNO055_I2C_bus_read;
-    bno055.delay_msec = BNO055_delay_msek;
-    bno055.dev_addr = BNO055_I2C_ADDR1;
-    return BNO055_INIT_VALUE;
-}
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+#define I2C_DEVICE_ADDRESS     b1110111
+#define DATA_READ_LENGTH 				10
+#define I2C3_NODE DT_NODELABEL(i2c3)
 
 void main(void) {
-
-	// Define starting values
-
-    struct bno055_t bno055;
-
-    s32 comres = BNO055_ERROR;
-    u8 power_mode = BNO055_INIT_VALUE;
-
-    s16 gyro_datax = BNO055_INIT_VALUE;
-    s16 gyro_datay = BNO055_INIT_VALUE;
-    s16 gyro_dataz = BNO055_INIT_VALUE;
-    struct bno055_gyro_t gyro_xyz;
-
-    double d_gyro_datax = BNO055_INIT_VALUE;
-    double d_gyro_datay = BNO055_INIT_VALUE;
-    double d_gyro_dataz = BNO055_INIT_VALUE;
-    struct bno055_gyro_double_t d_gyro_xyz;
-
-    // Initialize the BNO055 device
-
-    I2C_routine();
-
-    comres = bno055_init(&bno055);
-    power_mode = BNO055_POWER_MODE_NORMAL;
-    comres += bno055_set_power_mode(power_mode);
-
-    // Read the raw sensor data
-
-    comres += bno055_set_operation_mode(BNO055_OPERATION_MODE_GYRONLY);
-
-    comres += bno055_read_gyro_x(&gyro_datax);
-    comres += bno055_read_gyro_y(&gyro_datay);
-    comres += bno055_read_gyro_z(&gyro_dataz);
-    comres += bno055_read_gyro_xyz(&gyro_xyz);
-
-    // Read the converted sensor data
-
-    comres += bno055_convert_double_gyro_x_dps(&d_gyro_datax);
-    comres += bno055_convert_double_gyro_y_dps(&d_gyro_datay);
-    comres += bno055_convert_double_gyro_z_dps(&d_gyro_dataz);
-    comres += bno055_convert_double_gyro_x_rps(&d_gyro_datax);
-    comres += bno055_convert_double_gyro_y_rps(&d_gyro_datay);
-    comres += bno055_convert_double_gyro_z_rps(&d_gyro_dataz);
-    comres += bno055_convert_double_gyro_xyz_dps(&d_gyro_xyz);
-    comres += bno055_convert_double_gyro_xyz_rps(&d_gyro_xyz);
-
-    // De-Initialize the DNO055 device
-
-    power_mode = BNO055_POWER_MODE_SUSPEND;
-    comres += bno055_set_power_mode(power_mode);
-
-    // 
+	// Warning: if bno055 and bmp388 both use I2C3, which device will i2c_dev this bind to?
+	const struct device *i2c_dev = device_get_binding(DT_LABEL(I2C3_NODE));
+	struct sensor_value gyro[3];
 
 
+	printk("BNO055 sensor application\n");
+
+	if (i2c_dev == NULL) {
+		printk("Failed to get device binding");
+	}
+
+	while (1) {
+		if (sensor_sample_fetch(i2c_dev) < 0) {
+			printk("sensor: sample fetch fail.\n");
+			return;
+		}
+
+		if (sensor_channel_get(i2c_dev, SENSOR_CHAN_GYRO_XYZ, gyro) < 0) {
+			printk("sensor: channel get fail.\n");
+			return;
+		}
+
+		printf("bno055: gyro (dps): x: %.3f, y: %.3f, z: %.3f\n",
+			sensor_value_to_double(&gyro[0]),
+			sensor_value_to_double(&gyro[1]),
+			sensor_value_to_double(&gyro[2]));
+
+		k_msleep(SLEEP_TIME_MS);
+	}
 
 }
