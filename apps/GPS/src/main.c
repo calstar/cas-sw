628629262a40baa3bc752cce18f506cb3a2de463
Cooper Collier <collier@berkeley.edu>
2022-06-04 20:31:35 -0700

    made simple application but doesn't yet compile

--- apps/GPS/src/main.c
+++ apps/GPS/src/main.c
@@ -7,48 +7,28 @@
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
-#include <drivers/i2c.h>
+#include "../../cas-zephyr-drivers/zephyr/drivers/SAM_M8Q_driver/gnss.h"
 
-#define I2C_NODE i2c2
-#define DEVICE_I2C_ADDR 0x42
-
-// The sam-m8q has three registers (0xFD, 0xFE, and 0xFF) which can be
-// read through I2C. The 0xFF register contains the current data stream.
-#define DATA_REG 0xFF
-
-struct device* initialize_i2c_device() {
-	struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(I2C_NODE));
-	if (i2c_dev == NULL) {
-		printk("Failed to get device binding.\n");
-		return NULL;
-	}
-	if (!device_is_ready(i2c_dev)) {
-		printk("Device is not ready.\n");
-		return NULL;
-	}
-	return i2c_dev;
-}
-
-int ddc_read(struct device* i2c_device, int* read_buf) {
-
-}
-
-int ddc_write(struct device* i2c_device, int* write_buf) {
-
-}
-
-int ubx_configure(struct device* i2c_device) {
-
-}
+#define DEVICE_NODE sam_m8q_node
 
 void main(void) {
 
-	int rc;
-	struct device* i2c_dev = initialize_i2c_device();
-	if (i2c_dev == NULL) {
-		printk("Failed to initialize device.\n");
+	struct device *dev = DEVICE_DT_GET(DT_NODELABEL(DEVICE_NODE));
+	if (dev == NULL) {
+		printk("Failed to get device binding.\n");
 		return;
 	}
+	if (!device_is_ready(dev)) {
+		printk("Device is not ready.\n");
+		return;
+	}
+	if (gnss_enable(dev, NULL) != 0) {
+		printk("Error in enabling device.");
+		return;
+	}
+
+	printk("Sucesfully initialized device");
+	return;
 
 }
 
