9acd26a1e907a815faf2f2bc8f6e81722cad9d3b
Cooper Collier <collier@berkeley.edu>
2022-06-09 17:29:55 -0700

    compilation error with bmp388 driver

---
+++ apps/bmp388-breakout/src/main.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ // Documentation: https://docs.zephyrproject.org/3.0.0/reference/devicetree/bindings/sensor/bosch%2Cbmp388-i2c.html#dtbinding-bosch-bmp388-i2c
+
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/sensor.h>
+
+#define DEVICE_NODE bmp388_i2c_node
+
+void main(void) {
+
+	printk("BMP388 sensor application.\n");
+
+	const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(DEVICE_NODE));
+	struct sensor_value val;
+
+	if (i2c_dev == NULL) {
+		printk("Failed to get device binding.\n");
+		return;
+	} else {
+		printk("Got device binding.\n");
+	}
+
+	if (!device_is_ready(i2c_dev)) {
+		printk("Device is not ready.\n");
+		return;
+	} else {
+		printk("Device is ready.\n");
+	}
+
+	while (1) {
+
+		k_msleep(1000);
+
+		if (sensor_sample_fetch(i2c_dev) != 0) {
+			printk("Sensor sample fetch fail.\n");
+			continue;
+		} else {
+			printk("Sensor sample fetch succeeded.\n");
+		}
+
+		if (sensor_channel_get(i2c_dev, SENSOR_CHAN_PRESS, &val) != 0) {
+			printk("Sensor channel get fail.\n");
+			continue;
+		} else {
+			printk("Sensor channel get succeeded.\n");
+		}
+
+		printk("sensor: pressure reading: %d kilopascal.\n", val.val1);
+
+	}
+
+}
