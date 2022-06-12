ade32efecbef663fce6960e87a692e3d203feaf5
Cooper Collier <collier@berkeley.edu>
2022-06-11 21:18:31 -0700

    simplified spi testing function

--- apps/rfm69/src/main.c
+++ apps/rfm69/src/main.c
@@ -1,58 +1,70 @@
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
-#include "rfm69.h"
-#include "cas_spi.h"
+#include <drivers/gpio.h>
+#include <drivers/spi.h>
 
-// This application uses spi bus number 2, aka cas_spi0
-#define SPI_NODE cas_spi0
+void dw_transceive(const struct device *spi, struct spi_config* spi_cfg, uint8_t command, uint8_t rx_buf[], uint8_t rx_len) {
+        uint8_t tx_buf[1] = {command};
+        const struct spi_buf spi_buf_tx = {
+                .buf = tx_buf,
+                .len = sizeof(tx_buf)
+        };
+        struct spi_buf_set tx = {
+                .buffers = &spi_buf_tx,
+                .count = 1
+        };
 
-// Usually, this should be either 433 MHz or 915 MHz
-#define FREQUENCY RF69_915MHZ
-
-#define NETWORKID 0
-#define MYNODEID 0
-#define TONODEID 255
-
-uint8_t send_buffer[PAYLOAD_LENGTH];
-
-void clear_send_buffer() {
-	for (int i=0; i<PAYLOAD_LENGTH; i++) {
-		send_buffer[i] = 0;
-	}
+        struct spi_buf spi_buf_rx[] = {
+                {
+                        .buf = NULL,
+                        .len = sizeof(tx_buf)
+                },
+                {
+                        .buf = rx_buf,
+                        .len = rx_len
+                }
+        };
+        struct spi_buf_set rx = {
+                .buffers = spi_buf_rx,
+                .count = 2
+        };
+        int status = spi_transceive(spi, spi_cfg, &tx, &rx);
+        printk("Status: %d", status);
 }
 
 void main(void) {
 
 	printk(" --- RFM69 Radio Transciever ---\n");
 
-	struct device *dev = DEVICE_DT_GET(DT_NODELABEL(SPI_NODE));
+	// This application uses pin B9 as the CS pin
+	const struct spi_cs_control ctrl = {
+    	.gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpiob)),
+    	.gpio_pin = 9,
+    	.gpio_dt_flags = GPIO_ACTIVE_LOW,
+    	.delay = 10,
+    };
 
-	if (dev == NULL) {
+	const struct spi_config cfg = {
+		.frequency = 3600000U,
+		.operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8),
+		.cs = &ctrl,
+	};
+
+	// This application uses SPI bus 2, aka cas_spi0
+	const struct device *spi_dev = device_get_binding("SPI2");
+
+	if (spi_dev == NULL) {
 		printk("Error in main.c: Failed to get device binding.\n");
 		return NULL;
 	}
-	if (!device_is_ready(dev)) {
+	if (!device_is_ready(spi_dev)) {
 		printk("Error in main.c: Device is not ready.\n");
 		return NULL;
 	}
 
-	const struct spi_config cfg = {
-		.frequency = 4000000,
-		.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
-		.slave = 0,
-	};
+	uint8_t recieve_buffer[4] = { 0x00, 0x00, 0x00, 0x00 };
 
-	clear_send_buffer();
-	send_buffer[0] = 0xDE;
-	send_buffer[1] = 0xAD;
-	send_buffer[2] = 0xBE;
-	send_buffer[3] = 0xEF;
-
-	// Remove this line
-	while (1) { cas_spi_transceive(dev, &cfg, &send_buffer, 4, NULL, 0); }
-
-	// Uncomment this line
-	// transmit_packet(dev, cfg, &send_buffer, NETWORKID, TONODEID);
+	dw_transceive(spi_dev, &cfg, 0x55, &recieve_buffer, 4);
 	
 }
